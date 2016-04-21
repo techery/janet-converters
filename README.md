bcd9714e323ff2bee490f9e771bbc5c2cb8e3231
Aleksey Malevaniy <a.malevaniy@gmail.com>
2016-04-21 16:10:43 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Janet Converters
+Set of popular object-to-bytes converters for [Janet Services](https://github.com/techery/janet).
+
+Commonly used for network services:
+e.g. [Http service](https://github.com/techery/janet-http) and [Socket Service](https://github.com/techery/janet-sockets). 
+
+### Available converters
+#### Gson
+```groovy
+compile 'com.github.techery.janet-converters:gson:1.0.0'
+```
+#### Protobuf
+```groovy
+compile 'com.github.techery.janet-converters:protobuf:1.0.0'
+```
---
+++ base-body/src/main/java/io/techery/janet/body/StringBody.java
@@ -0,0 +1,36 @@
+package io.techery.janet.body;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class StringBody extends BytesArrayBody {
+
+    public static final String MIMETYPE = "text/plain; charset=UTF-8";
+    private final String string;
+
+    public StringBody(String string) {
+        super(MIMETYPE, convertToBytes(string));
+        this.string = string;
+    }
+
+    private static byte[] convertToBytes(String string) {
+        try {
+            return string.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    @Override public String toString() {
+        try {
+            return "StringBody[" + new String(getContent(), "UTF-8") + "]";
+        } catch (IOException e) {
+            throw new AssertionError("Must be able to decode UTF-8");
+        }
+    }
+
+}
