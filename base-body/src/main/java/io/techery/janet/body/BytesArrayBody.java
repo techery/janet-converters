4ca4a4f358cbb68fe377b96454f21b5300dcc8c7
Aleksey Malevaniy <a.malevaniy@gmail.com>
2017-05-24 19:05:51 +0300

    Make ActionBody public API to be more strict and stream oriented

--- base-body/src/main/java/io/techery/janet/body/BytesArrayBody.java
+++ base-body/src/main/java/io/techery/janet/body/BytesArrayBody.java
@@ -1,20 +1,52 @@
 package io.techery.janet.body;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
 
 public class BytesArrayBody extends ActionBody {
 
     private final byte[] bytes;
 
-    public BytesArrayBody(String mimetype, byte[] bytes) {
-        super(mimetype);
-        if (bytes == null) {
-            throw new NullPointerException("bytes == null");
-        }
+    public BytesArrayBody(String mimeType, byte[] bytes) {
+        super(mimeType);
+        if (bytes == null) throw new IllegalArgumentException("byte[] is null");
         this.bytes = bytes;
     }
 
-    @Override public byte[] getContent() throws IOException {
-        return bytes;
+    @Override public long length() {
+        return bytes.length;
+    }
+
+    @Override public InputStream getContent() {
+        return new ByteArrayInputStream(bytes);
+    }
+
+    @Override public void writeContentTo(OutputStream os) throws IOException {
+        os.write(bytes);
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        BytesArrayBody that = (BytesArrayBody) o;
+
+        return Arrays.equals(bytes, that.bytes);
+    }
+
+    @Override public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + Arrays.hashCode(bytes);
+        return result;
+    }
+
+    @Override public String toString() {
+        return "BytesArrayBody{" +
+                "bytes length=" + length() +
+                "} " + super.toString();
     }
 }
